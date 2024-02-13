0ac5e0c622fbc76d210a2d39e0cc9d9dda355e1b
PuaZhiXian <xianpua2001@gmail.com>
2024-02-13 15:19:12 +0800

    add trie & sorting test case

--- src/mainClass.java
+++ src/mainClass.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+
 public class mainClass {
 
     public static void main(String[] args) {
+        mainClass.trieTestCase();
+        mainClass.sortingTestCase();
+    }
+
+    public static void trieTestCase() {
+        System.out.println("\nTrie Test Case --------------");
         TrieNode trie = new TrieNode();
         //Test Case
         //Insert word to trie
@@ -18,9 +26,9 @@ public class mainClass {
         System.out.println("word 'ants' is present in Trie " + trie.search("ants"));
         System.out.println("word 'border' is present in Trie " + trie.search("border"));
         //search prefix
-        System.out.println("\nprefix 'an' is present in Trie "+ trie.isPrefixExist("an"));
-        System.out.println("prefix 'cat' is present in Trie "+ trie.isPrefixExist("car"));
-        System.out.println("prefix 'dr' is present in Trie "+ trie.isPrefixExist("dr"));
+        System.out.println("\nprefix 'an' is present in Trie " + trie.isPrefixExist("an"));
+        System.out.println("prefix 'cat' is present in Trie " + trie.isPrefixExist("car"));
+        System.out.println("prefix 'dr' is present in Trie " + trie.isPrefixExist("dr"));
 
         //delete word
         System.out.println("\ndelete word 'and' from Trie " + trie.deleteKey("and"));
@@ -29,6 +37,19 @@ public class mainClass {
 
         //Check delete status
         System.out.println("word 'and' is present in Trie " + trie.search("and"));
-
     }
+
+    public static void sortingTestCase() {
+        System.out.println("\nSorting Test Case --------------");
+        Sorting obj = new Sorting();
+
+        //radix sorting
+        int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+        System.out.println(Arrays.toString(obj.radixSort(arr)));
+
+        //counting sort
+        int[] singleNumberArr = {2, 5, 3, 0, 2, 3, 0, 3, 90};
+        System.out.println(Arrays.toString(obj.countingSort(singleNumberArr)));
+    }
+
 }
